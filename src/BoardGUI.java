83dfa48aed6e88727095bade8a92ae4bf28dd7a5
ruanbotha33 <ruanbotha33@gmail.com>
2016-11-25 13:43:01 -0700

    Changed Dimensions

--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -37,7 +37,8 @@ public class BoardGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 732, 603);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 609, 543);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -50,7 +51,7 @@ public class BoardGUI {
 		label.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label = new GridBagConstraints();
 		gbc_label.insets = new Insets(0, 0, 5, 5);
-		gbc_label.gridx = 2;
+		gbc_label.gridx = 0;
 		gbc_label.gridy = 0;
 		frame.getContentPane().add(label, gbc_label);
 		
@@ -58,7 +59,7 @@ public class BoardGUI {
 		label_1.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_1 = new GridBagConstraints();
 		gbc_label_1.insets = new Insets(0, 0, 5, 5);
-		gbc_label_1.gridx = 3;
+		gbc_label_1.gridx = 1;
 		gbc_label_1.gridy = 0;
 		frame.getContentPane().add(label_1, gbc_label_1);
 		
@@ -66,7 +67,7 @@ public class BoardGUI {
 		label_2.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/TilesEvent.png")));
 		GridBagConstraints gbc_label_2 = new GridBagConstraints();
 		gbc_label_2.insets = new Insets(0, 0, 5, 5);
-		gbc_label_2.gridx = 4;
+		gbc_label_2.gridx = 2;
 		gbc_label_2.gridy = 0;
 		frame.getContentPane().add(label_2, gbc_label_2);
 		
@@ -74,7 +75,7 @@ public class BoardGUI {
 		label_3.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_3 = new GridBagConstraints();
 		gbc_label_3.insets = new Insets(0, 0, 5, 5);
-		gbc_label_3.gridx = 5;
+		gbc_label_3.gridx = 3;
 		gbc_label_3.gridy = 0;
 		frame.getContentPane().add(label_3, gbc_label_3);
 		
@@ -82,15 +83,15 @@ public class BoardGUI {
 		label_4.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_4 = new GridBagConstraints();
 		gbc_label_4.insets = new Insets(0, 0, 5, 5);
-		gbc_label_4.gridx = 6;
+		gbc_label_4.gridx = 4;
 		gbc_label_4.gridy = 0;
 		frame.getContentPane().add(label_4, gbc_label_4);
 		
 		JLabel label_5 = new JLabel("");
 		label_5.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_5 = new GridBagConstraints();
-		gbc_label_5.insets = new Insets(0, 0, 5, 0);
-		gbc_label_5.gridx = 7;
+		gbc_label_5.insets = new Insets(0, 0, 5, 5);
+		gbc_label_5.gridx = 5;
 		gbc_label_5.gridy = 0;
 		frame.getContentPane().add(label_5, gbc_label_5);
 		
@@ -98,15 +99,15 @@ public class BoardGUI {
 		label_17.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_17 = new GridBagConstraints();
 		gbc_label_17.insets = new Insets(0, 0, 5, 5);
-		gbc_label_17.gridx = 2;
+		gbc_label_17.gridx = 0;
 		gbc_label_17.gridy = 1;
 		frame.getContentPane().add(label_17, gbc_label_17);
 		
 		JLabel label_6 = new JLabel("");
 		label_6.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_6 = new GridBagConstraints();
-		gbc_label_6.insets = new Insets(0, 0, 5, 0);
-		gbc_label_6.gridx = 7;
+		gbc_label_6.insets = new Insets(0, 0, 5, 5);
+		gbc_label_6.gridx = 5;
 		gbc_label_6.gridy = 1;
 		frame.getContentPane().add(label_6, gbc_label_6);
 		
@@ -114,15 +115,15 @@ public class BoardGUI {
 		label_16.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_16 = new GridBagConstraints();
 		gbc_label_16.insets = new Insets(0, 0, 5, 5);
-		gbc_label_16.gridx = 2;
+		gbc_label_16.gridx = 0;
 		gbc_label_16.gridy = 2;
 		frame.getContentPane().add(label_16, gbc_label_16);
 		
 		JLabel label_7 = new JLabel("");
 		label_7.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/TilesEvent.png")));
 		GridBagConstraints gbc_label_7 = new GridBagConstraints();
-		gbc_label_7.insets = new Insets(0, 0, 5, 0);
-		gbc_label_7.gridx = 7;
+		gbc_label_7.insets = new Insets(0, 0, 5, 5);
+		gbc_label_7.gridx = 5;
 		gbc_label_7.gridy = 2;
 		frame.getContentPane().add(label_7, gbc_label_7);
 		
@@ -130,15 +131,15 @@ public class BoardGUI {
 		label_15.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_15 = new GridBagConstraints();
 		gbc_label_15.insets = new Insets(0, 0, 5, 5);
-		gbc_label_15.gridx = 2;
+		gbc_label_15.gridx = 0;
 		gbc_label_15.gridy = 3;
 		frame.getContentPane().add(label_15, gbc_label_15);
 		
 		JLabel label_8 = new JLabel("");
 		label_8.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_8 = new GridBagConstraints();
-		gbc_label_8.insets = new Insets(0, 0, 5, 0);
-		gbc_label_8.gridx = 7;
+		gbc_label_8.insets = new Insets(0, 0, 5, 5);
+		gbc_label_8.gridx = 5;
 		gbc_label_8.gridy = 3;
 		frame.getContentPane().add(label_8, gbc_label_8);
 		
@@ -146,7 +147,7 @@ public class BoardGUI {
 		label_14.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/TilesEvent.png")));
 		GridBagConstraints gbc_label_14 = new GridBagConstraints();
 		gbc_label_14.insets = new Insets(0, 0, 0, 5);
-		gbc_label_14.gridx = 2;
+		gbc_label_14.gridx = 0;
 		gbc_label_14.gridy = 4;
 		frame.getContentPane().add(label_14, gbc_label_14);
 		
@@ -154,7 +155,7 @@ public class BoardGUI {
 		label_13.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_13 = new GridBagConstraints();
 		gbc_label_13.insets = new Insets(0, 0, 0, 5);
-		gbc_label_13.gridx = 3;
+		gbc_label_13.gridx = 1;
 		gbc_label_13.gridy = 4;
 		frame.getContentPane().add(label_13, gbc_label_13);
 		
@@ -162,7 +163,7 @@ public class BoardGUI {
 		label_12.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_12 = new GridBagConstraints();
 		gbc_label_12.insets = new Insets(0, 0, 0, 5);
-		gbc_label_12.gridx = 4;
+		gbc_label_12.gridx = 2;
 		gbc_label_12.gridy = 4;
 		frame.getContentPane().add(label_12, gbc_label_12);
 		
@@ -170,7 +171,7 @@ public class BoardGUI {
 		label_11.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_11 = new GridBagConstraints();
 		gbc_label_11.insets = new Insets(0, 0, 0, 5);
-		gbc_label_11.gridx = 5;
+		gbc_label_11.gridx = 3;
 		gbc_label_11.gridy = 4;
 		frame.getContentPane().add(label_11, gbc_label_11);
 		
@@ -178,14 +179,15 @@ public class BoardGUI {
 		label_10.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_10 = new GridBagConstraints();
 		gbc_label_10.insets = new Insets(0, 0, 0, 5);
-		gbc_label_10.gridx = 6;
+		gbc_label_10.gridx = 4;
 		gbc_label_10.gridy = 4;
 		frame.getContentPane().add(label_10, gbc_label_10);
 		
 		JLabel label_9 = new JLabel("");
 		label_9.setIcon(new ImageIcon(BoardGUI.class.getResource("/Resources/Tile.png")));
 		GridBagConstraints gbc_label_9 = new GridBagConstraints();
-		gbc_label_9.gridx = 7;
+		gbc_label_9.insets = new Insets(0, 0, 0, 5);
+		gbc_label_9.gridx = 5;
 		gbc_label_9.gridy = 4;
 		frame.getContentPane().add(label_9, gbc_label_9);
 		frame.setVisible(true);
